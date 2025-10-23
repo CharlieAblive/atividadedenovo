199d5f032e5be757fc6bf5e699f839497a02cef7
CharlieAblive <charlieablive@gmail.com>
2025-10-23 22:19:48 +0000

    adicionei comentarios

--- Projeto/src/App.java
+++ Projeto/src/App.java
@@ -2,15 +2,16 @@ import java.util.Scanner;
 import funcionario.*;
 public class App {
     public static void main(String[] args) {
-        
         Horista horista1 = new Horista(null, 0, 0, 0);
         Comissionado comissionado1 = new Comissionado(null, 0, 0, 0);
         Assalariado assalariado1 = new Assalariado(null, 0, 0, 0);
         Scanner input = new Scanner(System.in);
         Scanner inputString = new Scanner(System.in);
         int opcao;
+
+        //menu 
         do {
-            System.out.println("Menu de Funcionários");
+            System.out.println("~~~Menu de Funcionários~~~");
             System.out.println("Escolha uma opção:");
             System.out.println("1- Cadastrar novo funcionario");
             System.out.println("2- Exibir informações dos funcionarios");
@@ -18,6 +19,7 @@ public class App {
             System.out.println("4- Exibir funcionario com maior pagamento");
             System.out.println("5- Sair");
             opcao = input.nextInt();
+            //switch case pras opcoes
             switch (opcao) {
                 case 1:
                     System.out.println("Selecione o tipo de funcionário:");
@@ -25,7 +27,9 @@ public class App {
                     System.out.println("2- Comissionado");
                     System.out.println("3- Assalariado");
                     int opcaoCase1 = input.nextInt();
+                    //switch case especifico dos funcionarios
                     switch (opcaoCase1) {
+                        //funcionario horista
                         case 1:
                             System.out.println("Digite as informações do novo funcionario horista: ");
                             System.out.println("Nome: ");
@@ -37,7 +41,9 @@ public class App {
                             System.out.println("Horas trabalhadas no Mês: ");
                             horista1.setHorasTrabalhadas(input.nextInt());  
                             horista1 = new Horista(horista1.getNome(), horista1.getId(), horista1.getSalario(), horista1.getHorasTrabalhadas());  
+                            System.out.println("Funcionario adicionado.");
                             break;
+                        //funcionario comissionado
                         case 2:
                             System.out.println("Digite as informações do novo funcionario comissionado: ");
                             System.out.println("Nome: ");
@@ -49,7 +55,9 @@ public class App {
                             System.out.println("Total de vendas no mês ");
                             comissionado1.setVendas(input.nextInt());  
                             comissionado1 = new Comissionado(comissionado1.getNome(), comissionado1.getId(), comissionado1.getSalario(), comissionado1.getVendas());  
+                            System.out.println("Funcionario adicionado.");
                             break;
+                        //funcionario assalariado
                         case 3:
                             System.out.println("Digite as informações do novo funcionario assalariado: ");
                             System.out.println("Nome: ");
@@ -61,21 +69,28 @@ public class App {
                             System.out.println("Total de vendas no mês ");
                             assalariado1.setBonus(input.nextInt());  
                             assalariado1 = new Assalariado(assalariado1.getNome(), assalariado1.getId(), assalariado1.getSalario(), assalariado1.getBonus());  
+                            System.out.println("Funcionario adicionado.");
                             break;
                         default:
                             System.out.println("Opção Inválida");
                             break;
                     }
                     break;
+
+                //informacoes do funcionario
                 case 2:
                     System.out.println(horista1.getNome() + " " + horista1.getId() + " " + horista1.getSalario() + ".");
                     System.out.println(comissionado1.getNome() + " " + comissionado1.getId() + " " + comissionado1.getSalario() + ".");
                     System.out.println(assalariado1.getNome() + " " + assalariado1.getId() + " " + assalariado1.getSalario() + ".");
                     break;
+
+                //gasto total
                 case 3:
                     double gastoTotal = horista1.getSalario() + comissionado1.getSalario() + assalariado1.getSalario();
                     System.out.println("O total de gastos da empresa para este mês é de R$" + gastoTotal + ".");
                     break;
+
+                //funcionario com maior salario
                 case 4:
                     if (horista1.getSalario() > comissionado1.getSalario() && horista1.getSalario() > assalariado1.getSalario()) {
                         System.out.println("O funcionario " + horista1.getNome() + " terá o maior salario este mês." );
